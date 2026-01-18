b33897a23f763476a317618df333c499668256c2
nsahithi04 <sahithi.nampally@gmail.com>
2026-01-18 11:09:58 -0700

    added structured response format

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import List
 from dotenv import load_dotenv
 from langchain.agents import create_agent
 from langchain.tools import tool
@@ -5,11 +6,12 @@ from langchain_core.messages import HumanMessage
 from langchain_ollama import ChatOllama
 # from tavily import TavilyClient
 from langchain_tavily import TavilySearch
+from pydantic import BaseModel, Field
 
 load_dotenv()
 # tavily = TavilyClient()
 
-
+# this is a search tool defined by us but we can also use existing search function in tvaily for better answers
 # @tool
 # def search(query: str) -> str:
 #     """this tools is a search engine that searched the query over the internet and returns a valid respose
@@ -19,19 +21,39 @@ load_dotenv()
 #     print(f"searching for {query}")
 #     return tavily.search(query=query)
 
+#how to structure fromat into answers and sources
+# class Source(BaseModel):
+#     """schema for the sources used by agent"""
 
+#     url: str = Field(description="The URL of the source")
+
+
+# class StructuredResponse(BaseModel):
+#     """the response of the agent for the query"""
+
+#     answer: str = Field(description="the agents answer to the query")
+#     Sources: list[Source] = Field(
+#         default_factory=list, description="list of sources used for the response"
+#     )
+
+# structured format does not work for ollama
 llm = ChatOllama(
     model="gpt-oss:120b-cloud",
 )
-tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
 
+tools = [TavilySearch()]
+# agent = create_agent(model=llm, tools=tools, response_format=StructuredResponse)
+agent = create_agent(model=llm, tools=tools)
 
 def main():
     print("Hello from langchain-course!")
 
     result = agent.invoke(
-        {"messages": HumanMessage(content="what is the weather in india?")}
+        {
+            "messages": HumanMessage(
+                content="search for 3 job posting for a AI engineer using langchain in Arizona on linked in and list their response"
+            )
+        }
     )
     print(result)
 
