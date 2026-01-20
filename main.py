a27250e4a12dcad86b14658656c34f8a673d0f04
nsahithi04 <sahithi.nampally@gmail.com>
2026-01-19 18:12:01 -0700

    used with_structured_output

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from dotenv import load_dotenv
 from langchain_classic import hub
 from langchain_classic.agents import AgentExecutor
 from langchain_classic.agents.react.agent import create_react_agent
-from langchain_core.output_parsers.pydantic import PydanticOutputParser
+# from langchain_core.output_parsers.pydantic import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableLambda
+from langchain_google_genai import ChatGoogleGenerativeAI
 # from langchain_ollama import ChatOllama
 from langchain_tavily import TavilySearch
-from langchain_google_genai import ChatGoogleGenerativeAI
 
 from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
 from schemas import AgentResponse
@@ -20,11 +20,14 @@ tools = [TavilySearch()]
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite")
 react_prompt = hub.pull("hwchase17/react")
 
-output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+# output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+
+structured_llm = llm.with_structured_output(AgentResponse)
+
 react_prompt_with_format_instructions = PromptTemplate(
     template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
     input_variables=["input", "agent_scratchpad", "tool_names"],
-).partial(format_instructions=output_parser.get_format_instructions())
+).partial(format_instructions="")
 
 agent = create_react_agent(
     llm=llm,
@@ -38,8 +41,8 @@ agent_executor = AgentExecutor(
     verbose=True,
 )
 extract_output = RunnableLambda(lambda x: x["output"])
-parse_output = RunnableLambda(lambda x: output_parser.parse(x))
-chain = agent_executor | extract_output | parse_output
+# parse_output = RunnableLambda(lambda x: output_parser.parse(x))
+chain = agent_executor | extract_output | structured_llm
 
 
 def main():
